ebbf6273fa57e38c7513368f11090f29decc4fab
Yusef Tohamy <tuo13985@temple.edu>
2025-02-19 15:25:16 -0500

    names was declared as List instead of MutableList

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -12,7 +12,7 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var names: List<String>
+    lateinit var names: MutableList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
